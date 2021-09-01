25bef3ee7efcc6789db7d310948be89cb472d608
Matt Chan <matt.chan120@gmail.com>
2021-08-31 22:20:43 -0400

    fiddling with mysql and database creation

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+// running after server starts
+app.listen("3000", () => {
+  console.log("Server started on port 3000!");
+});
 
 // create connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password'
-    // database : 'hackathon_db'
+  host: "localhost",
+  user: "root",
+  password: "password",
+  // database : 'hackathon_db'
 });
 
 // connecting to the db
 db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('MySQL Connected')
+  if (err) {
+    throw err;
+  }
+  console.log("MySQL Connected!");
 });
-
-const app = express();
 
 // creating database with mysql npm
-app.get('/createdatabase', (req, res) => {
-    let sql = 'CREATE DATABASE hackathon_db';
-    db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result)
-        res.send('Database created!')
-    })
-} )
-
-app.listen('3000', () => {
-    console.log('Server started on port 3000!')
-});
+// app.get('/createdatabase', (req, res) => {
+//   let sql = 'CREATE DATABASE hackathon_db';
+//   db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send("Database created!");
+//   });
+// });
