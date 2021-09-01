1b11831ca973a4683202560494449028ef0ec668
Matt Chan <matt.chan120@gmail.com>
2021-08-31 22:49:47 -0400

    cannot connect to the db

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 const express = require("express");
 const mysql = require("mysql");
 
-const app = express();
-
-// running after server starts
-app.listen("3000", () => {
-  console.log("Server started on port 3000!");
-});
-
 // create connection
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
-  // database : 'hackathon_db'
+  password: "password"
 });
 
 // connecting to the db
 db.connect((err) => {
-  if (err) {
+  if(err) {
     throw err;
   }
   console.log("MySQL Connected!");
 });
 
-// creating database with mysql npm
-// app.get('/createdatabase', (req, res) => {
-//   let sql = 'CREATE DATABASE hackathon_db';
-//   db.query(sql, (err, result) => {
-//     if (err) throw err;
-//     console.log(result);
-//     res.send("Database created!");
-//   });
-// });
+const app = express();
+
+// creating database with mysql 
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE petsmysql';
+    db.query(sql, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send("Database created!");
+    });
+  });
+
+// running after server starts
+app.listen("3306", () => {
+  console.log("Server started on port 3306!");
+});
