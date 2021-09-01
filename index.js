8b56ce96dd4d4faf9fe935642e65c1d5955e4953
Matt Chan <matt.chan120@gmail.com>
2021-08-31 22:10:40 -0400

    using mysql for database creation

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-import express from 'express'
-import cors from 'cors'
+const express = require('express');
+const mysql = require('mysql');
+
+// create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password'
+    // database : 'hackathon_db'
+});
+
+// connecting to the db
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL Connected')
+});
 
 const app = express();
 
-// using cors
-app.use(cors());
+// creating database with mysql npm
+app.get('/createdatabase', (req, res) => {
+    let sql = 'CREATE DATABASE hackathon_db';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result)
+        res.send('Database created!')
+    })
+} )
 
-// using express middleware for json parsing
-app.use(express.json());
-
-// for future db configuration
-const db = require ('./config/keys');
+app.listen('3000', () => {
+    console.log('Server started on port 3000!')
+});
