4e7f1f3900fefaa37e81048616da2a3ae4756fb6
Matt Chan <matt.chan120@gmail.com>
2021-09-07 19:59:05 -0400

    removing the listen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 
+
 // create connection
 const db = mysql.createConnection({
   host: 'localhost',
@@ -18,17 +19,17 @@ db.connect((err) => {
 
 const app = express();
 
+// running after server starts
+// app.listen("3306", () => {
+//   console.log("Server started on port 3306!");
+// });
+
 // creating database with mysql 
 app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE petsmysql';
+    let sql = 'CREATE DATABASE petsdb';
     db.query(sql, (err, result) => {
       if(err) throw err;
       console.log(result);
       res.send("Database created!");
     });
   });
-
-// running after server starts
-app.listen("3306", () => {
-  console.log("Server started on port 3306!");
-});
