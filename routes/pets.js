b77b3f7cb5cb1710e1ab439ae002e523c3dfa5c4
Matt Chan <matt.chan120@gmail.com>
2021-09-13 22:13:34 -0400

    just refactoring the pets routes

--- routes/pets.js
+++ routes/pets.js
@@ -1,80 +1,89 @@
-const express = require('express');
+const express = require("express");
 const mysql = require("mysql");
-const uuid = require('uuidv4')
+const uuid = require("uuidv4");
 
 const app = express();
 const router = express.Router();
 
 // create connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password123!',
-    database: 'pets_db'
-  });
+  host: "localhost",
+  user: "root",
+  password: "password123!",
+  database: "pets_db",
+});
 
 // select all pets
-  router.get('/getpets', (req, res) => {
-    let sql = "SELECT * FROM pets";
-    let query = db.query(sql, (err, results) => {
-      if (err) throw err;
-      console.log(results);
-      res.send('Pets fetched!')
-    });
+router.get("/getpets", (req, res) => {
+  let sql = "SELECT * FROM pets";
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.send("Pets fetched!");
   });
+});
 
 // select single pet
-  router.get('/getpets/:id', (req, res) => {
-    let sql = `SELECT * FROM pets WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.send('Pet fetched!')
-    });
-  });  
+router.get("/getpets/:id", (req, res) => {
+  let sql = `SELECT * FROM pets WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Pet fetched!");
+  });
+});
 
 // insert pet 1 test
-router.post('/addPet1', (req, res) => {
-    let pet = { name: 'Snowball', city: 'Toronto', description: 'white longhair', id: uuid.uuid() };
-    let sql = 'INSERT INTO pets SET ?';
-    let query = db.query(sql, pet, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.send(`${pet.name} added`)
-    })
-  });
-
-  // insert pet 2 test
-router.post('/addPet2', (req, res) => {
-  let pet = { name: 'Snowball II', city: 'Toronto', description: 'black longhair', id: uuid.uuid() };
-  let sql = 'INSERT INTO pets SET ?';
+router.post("/addPet1", (req, res) => {
+  let pet = {
+    name: "Snowball",
+    city: "Toronto",
+    description: "white longhair",
+    id: uuid.uuid(),
+  };
+  let sql = "INSERT INTO pets SET ?";
   let query = db.query(sql, pet, (err, result) => {
     if (err) throw err;
     console.log(result);
-    res.send(`${pet.name} added`)
-  })
+    res.send(`${pet.name} added`);
+  });
 });
 
-  
-  // update pet info
-  router.patch('/updatepet/:id', (req, res) => {
-    let newName = 'Cookie';
-    let sql = `UPDATE pets SET name = '${newName}' WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.send('Pet info Updated!')
-    });
+// insert pet 2 test
+router.post("/addPet2", (req, res) => {
+  let pet = {
+    name: "Snowball II",
+    city: "Toronto",
+    description: "black longhair",
+    id: uuid.uuid(),
+  };
+  let sql = "INSERT INTO pets SET ?";
+  let query = db.query(sql, pet, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(`${pet.name} added`);
   });
-  
-  // remove pet
-  router.delete('/deletepet/:id', (req, res) => {
-    let sql = `DELETE FROM pets WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.send('Pet Removed from Database!')
-    });
-  });
+});
 
-  module.exports = router;
+// update pet info
+router.patch("/updatepet/:id", (req, res) => {
+  let newName = "Cookie";
+  let sql = `UPDATE pets SET name = '${newName}' WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Pet info Updated!");
+  });
+});
+
+// remove pet
+router.delete("/deletepet/:id", (req, res) => {
+  let sql = `DELETE FROM pets WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Pet Removed from Database!");
+  });
+});
+
+module.exports = router;
