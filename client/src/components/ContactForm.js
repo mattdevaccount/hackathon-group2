4e93b24e7e268873a6ecaa9ac87d1966a4edcc6e
Matt Chan <matt.chan120@gmail.com>
2021-09-15 23:27:23 -0400

    starting contact form

---
+++ client/src/components/ContactForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+
+export default function ContactForm() {
+    const classes = useStyles();
+
+    return (
+        <form className={classes.root} noValidate autoComplete='off'>
+            <TextField variant='outlined' label='First Name'></TextField>
+            <TextField variant='outlined' label='Last Name'></TextField>
+        </form>
+    )
+}
