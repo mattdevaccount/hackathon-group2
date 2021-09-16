f8b99f96262304c0aa9c51b360877509f48baa1d
Rebecca Zeidenberg <rebeccazeidenberg@gmail.com>
2021-09-15 23:34:47 -0400

    updated login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,11 @@
 import React, { useState } from "react";
 import Cookies from 'js-cookie';
 import { useHistory } from "react-router-dom";
-import TextField from '@material-ui/core/TextField';
-import { Field, reduxForm } from 'redux-form';
-
-
+import { TextField, Button } from '@material-ui/core/';
 
 
 function Login() {
 
-    // const renderField = ({ input, label, type, meta: { touched, error } }) => (
-    //     <div>
-    //       <label>{label}</label>
-    //       <div>
-    //         <input {...input} placeholder={label} type={type}/>
-    //         {touched && error && <span>{error}</span>}
-    //       </div>
-    //     </div>
-    //   )
 
         return (
             <div className="LoginPage">
@@ -25,24 +13,19 @@ function Login() {
                     <h2>New Tails</h2>
                     <p>Welcome Back!</p>
                     <form>
-                        <label>Email</label>
                         <div>
-                            <Field name="email" component="input" type="text"/>
+                            <input name="email" component="input" type="text"/>
                         </div>
 
-                        <label>Password</label>
                         <div>
-                            <Field name="password" component="input" type="text"/>
+                            <input name="password" component="input" type="text"/>
                         </div>
 
-                        <button className="LoginButton">Login</button>
+                        <Button className="LoginButton" variant="contained">Login</Button>
                     </form>
                 </div>
             </div>
         )
 }
 
-Login = reduxForm({
-    form: 'loginForm'
-})(Login)
 export default Login;
