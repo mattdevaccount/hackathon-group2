ace1600197d571d0558f2623777da3fa15b41394
Matt Chan <matt.chan120@gmail.com>
2021-09-16 18:12:32 -0400

    starting on the form with text fields, and buttons, working on themes and colours

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,24 +1,43 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { createTheme } from "@mui/material/styles"
+import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+import { blue, orange, red } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
     },
-  }));
+  },
+}));
+
+const theme = createTheme({
+  palette: {
+    primary: orange,
+  },
+});
 
 export default function ContactForm() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <form className={classes.root} id='contact-form' noValidate autoComplete='off'>
-            <TextField variant='outlined' label='First Name'></TextField>
-            <TextField variant='outlined' label='Last Name'></TextField>
-            <TextField variant='outlined' label='Email'></TextField>
-        </form>
-    )
+  return (
+      <form className={classes.root} noValidate autoComplete="off" id='contact-form-root'>
+        <header className="contact-form-header">
+          <div className="contact-form-subheader1">Introduce Yourself!</div>
+          <div className="contact-form-subheader2">
+            The organization where your pet is housed will receive the details
+            below
+          </div>
+        </header>
+        <TextField variant="outlined" label="First Name" />
+        <TextField variant="outlined" label="Last Name" />
+        <TextField variant="outlined" label="Email" />
+        <TextField variant="outlined" label="Phone Number" />
+        <TextField variant="outlined" label="Message" multiline maxRows={4} />
+        <Button variant='contained' className='submitButton' color='primary'>Submit</Button>
+      </form>
+  );
 }
