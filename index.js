edae1c3978532850778f937fcef49d3ad25ccdb6
Matt Chan <matt.chan120@gmail.com>
2021-09-19 16:25:02 -0400

    added quick readme for start up

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.get("/createuserstable", () => {
 });
 
 // running after server starts
-app.listen("3000", () => {
-  console.log("Server started on port 3000!");
+app.listen("4000", () => {
+  console.log("Server started on port 4000!");
 });
