3bca5a2976a593ca8e08060daa8b27450a81de02
Rebecca Zeidenberg <rebeccazeidenberg@gmail.com>
2021-09-23 21:29:43 -0400

    working on login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,33 +1,39 @@
 import React, { useState } from "react";
 import Cookies from 'js-cookie';
 import { useHistory } from "react-router-dom";
-import { Button, Box } from '@material-ui/core/';
+import { Box } from '@material-ui/core/';
 
 
 function Login() {
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const clickHandler= () => {
+        console.log("hello")
+        console.log(email, password)
+    }
+
+    fetch()
 
     return (
         <div className="LoginPage">
-
             <Box className="LoginBox">
                 <h2>New Tails</h2>
                 <p>Welcome Back!</p>
-                <form>
-                    <div className="EmailTitle">
-                        <p>Email</p>
-                        <div className="EmailForm">
-                            <input className="email" component="input" type="text"/>
-                        </div>
+                <div className="EmailTitle">
+                    <p>Email</p>
+                    <div className="EmailForm">
+                        <input className="email" component="input" type="text" onChange={(event) => {setEmail(event.target.value)}}/>
                     </div>
+                </div>
 
-                    <div className="PasswordTitle">
-                        <p>Password</p>
-                        <input className="password" component="input" type="text"/>
-                    </div>
+                <div className="PasswordTitle">
+                    <p>Password</p>
+                    <input className="password" component="input" type="text" onChange={(event) => {setPassword(event.target.value)}}/>
+                </div>
 
-                    <button className="LoginButton" variant="contained">Login</button>
-                </form>
+                <button className="LoginButton" variant="contained" onClick={clickHandler}>Login</button>
             </Box>
         </div>
     )
