b5061d2935d243030d9cbe72f3dfc90900f94cde
Matt Chan <matt.chan120@gmail.com>
2021-09-27 17:44:37 -0400

    about to merge with login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,32 +1,6 @@
 import React, { useState } from "react";
 import Cookies from 'js-cookie';
 import { useHistory } from "react-router-dom";
-<<<<<<< HEAD
-import { Button, Box } from '@material-ui/core/';
-
-
-function Login() {
-    return (
-        <div className="LoginPage">
-
-            <Box className="LoginBox">
-                <h2>New Tails</h2>
-                <p>Welcome Back!</p>
-                <form>
-                    <div>
-                        <input name="email" component="input" type="text"/>
-                    </div>
-
-                    <div>
-                        <input name="password" component="input" type="text"/>
-                    </div>
-
-                    <Button className="LoginButton" variant="contained">Login</Button>
-                </form>
-            </Box>
-        </div>
-    )
-=======
 import TextField from '@material-ui/core/TextField';
 import { Field, reduxForm } from 'redux-form';
 
@@ -34,16 +8,6 @@ import { Field, reduxForm } from 'redux-form';
 
 
 function Login() {
-
-    // const renderField = ({ input, label, type, meta: { touched, error } }) => (
-    //     <div>
-    //       <label>{label}</label>
-    //       <div>
-    //         <input {...input} placeholder={label} type={type}/>
-    //         {touched && error && <span>{error}</span>}
-    //       </div>
-    //     </div>
-    //   )
 
         return (
             <div className="LoginPage">
@@ -66,7 +30,6 @@ function Login() {
                 </div>
             </div>
         )
->>>>>>> 56457897958738297fbeb1816d935b794bd249e3
 }
 
 Login = reduxForm({
