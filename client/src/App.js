4f3a8eafb1ae24fb5f2139ba8ef543d830b872a7
Matt Chan <matt.chan120@gmail.com>
2021-09-27 18:07:25 -0400

    merging frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,57 @@
-import './App.css';
-import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
-import Cookies from 'js-cookie';
-import Home from './components/Home';
-import About from './components/About';
-import Search from './components/Search';
-import Contact from './components/Contact';
-import Login from './components/Login';
-import LoggedIn from './components/LoggedIn';
-import Dashboard from './components/Dashboard';
-import Post from './components/Post';
-import Manage from './components/Manage';
+import "./App.scss";
 
-const isCookie = function() {
-  const cookie = Cookies.get("token");
-  return !!cookie;
-}
+import React, { useState, useEffect } from "react";
+import About from './components/About';
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+import PetProfile from "./components/PetProfile";
+import ContactForm from "./components/ContactForm";
+import Cookies from 'js-cookie';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
+  const [petPool, setPetPool] = useState([]);
+  const [petPoolTrigger, setPetPoolTrigger] = useState(true)
 
-  const [isLoggedIn, setIsLoggedIn] = useState(isCookie());
+  const [userPool, setUserPool] = useState([]);
+  const [userPoolTrigger, setUserPoolTrigger] = useState(true)
+
+  const getPetPool = async () => {
+    const response = await axios.get('http://localhost:4000/getPets');
+    console.log(response.data);
+    setPetPool(response.data);
+    setPetPoolTrigger(false);
+  }
+
+  const getUserPool = async () => {
+    const response = await axios.get('http://localhost:4000/getUsers');
+    setUserPool(response.data);
+    setUserPoolTrigger(false);
+  }
+
+  useEffect(() => {
+    if (petPoolTrigger === true) {
+      getPetPool();
+    }
+    if (userPoolTrigger === true) {
+      getUserPool();
+    }
+  });
 
   return (
     <Router>
-      <div className="Pages">
-        <Switch>
-          {/* <Route path="/Home">
-            <Home />
-          </Route> */}
-
-          {/* <Route path="/About">
-            <About />
-          </Route> */}
-
-          {/* <Route path="/Search">
-            <Search />
-          </Route> */}
-
-          {/* <Route path="/Contact">
-            <Contact />
-          </Route> */}
-
-          <Route path="/Login">
-            <Login setIsLoggedIn={setIsLoggedIn}/>
-          </Route>
-          
-          {/* <Route path="/Dashboard">
-            <Dashboard />
-          </Route> */}
-
-          {/* <Route path="/LoggedIn">
-            <LoggedIn {...isLoggedIn ? <Dashboard /> : <Redirect to="/Login" />} />
-          </Route> */}
-
-          {/* <Route path="Post">
-            <Post />
-          </Route> */}
-
-          {/* <Route path="Manage">
-            <Manage />
-          </Route> */}
-        </Switch>
-      </div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route 
+        path="/profile" 
+        component={PetProfile}
+        petPool={petPool} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={ContactForm} />
+      </Switch>
     </Router>
   );
 }
